7b2baf9b5e63fe9348201504433c283aaf3ac163
JPWilson2021 <wilsonjp805@gmail.com>
2022-10-16 14:47:17 -0400

    Added progress bar

--- app/src/main/java/com/jwilson/weatherapp/MainActivity.java
+++ app/src/main/java/com/jwilson/weatherapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.jwilson.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout homeRL;
+    private ProgressBar loadingPB;
+    private TextView cityNameTV, temperatureTV, conditionTV;
+    private TextInputEditText cityEdt;
+    private ImageView backIV, iconIV, searchIV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homeRL = findViewById(R.id.idRLHome);
+        homeRL = findViewById(R.id.idRLHome);
+        homeRL = findViewById(R.id.idRLHome);
+        homeRL = findViewById(R.id.idRLHome);
+        homeRL = findViewById(R.id.idRLHome);
+        homeRL = findViewById(R.id.idRLHome);
     }
 }
