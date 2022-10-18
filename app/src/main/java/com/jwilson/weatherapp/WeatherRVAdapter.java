1d8351a3f5b2a43912b134af39153e782092ae0d
JPWilson2021 <wilsonjp805@gmail.com>
2022-10-17 22:30:18 -0400

    Configured location manager

--- app/src/main/java/com/jwilson/weatherapp/WeatherRVAdapter.java
+++ app/src/main/java/com/jwilson/weatherapp/WeatherRVAdapter.java
@@ -1,8 +1,10 @@
 package com.jwilson.weatherapp;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,7 +12,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.ViewHolder>{
 
@@ -36,6 +41,14 @@ public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.View
         holder.temperatureTV.setText(modal.getTemperature() + "°c");
         Picasso.get().load("http:".concat(modal.getIcon())).into(holder.conditionIV);
         holder.windTV.setText(modal.getWindSpeed() + "Km/h");
+        SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        SimpleDateFormat output = new SimpleDateFormat("hh:mm aa");
+        try {
+            Date t = input.parse(modal.getTime());
+            holder.timeTV.setText(output.format(t));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 
